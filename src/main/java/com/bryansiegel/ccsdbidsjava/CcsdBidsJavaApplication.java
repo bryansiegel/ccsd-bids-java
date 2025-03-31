94871823fba83c2a2f74a95a747df573f948d4c8
Bryan Siegel <bryansiegel@gmail.com>
2025-03-31 08:46:02 -0700

    initial commit with login/dashboard and db setup

---
+++ src/main/java/com/bryansiegel/ccsdbidsjava/CcsdBidsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.bryansiegel.ccsdbidsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcsdBidsJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcsdBidsJavaApplication.class, args);
+    }
+
+}
