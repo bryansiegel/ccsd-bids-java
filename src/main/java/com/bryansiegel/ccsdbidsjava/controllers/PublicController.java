835de76740f2a05e26a826d7726997ad6c3f27e3
Bryan Siegel <bryansiegel@gmail.com>
2025-04-15 09:06:38 -0700

    mockup front end design

--- src/main/java/com/bryansiegel/ccsdbidsjava/controllers/PublicController.java
+++ src/main/java/com/bryansiegel/ccsdbidsjava/controllers/PublicController.java
@@ -1,13 +1,29 @@
 package com.bryansiegel.ccsdbidsjava.controllers;
 
+import com.bryansiegel.ccsdbidsjava.models.Bids;
+import com.bryansiegel.ccsdbidsjava.services.BidsService;
+import com.bryansiegel.ccsdbidsjava.services.SubContractorListingService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class PublicController {
 
+    private final BidsService bidsService;
+    private final SubContractorListingService subContractorListingService;
+
+    public PublicController(BidsService bidsService, SubContractorListingService subContractorListingService) {
+        this.bidsService = bidsService;
+        this.subContractorListingService = subContractorListingService;
+    }
+
     @GetMapping("/")
-    public String index() {
+    public String getBids(Model model) {
+        List<Bids> bids = bidsService.findAll(); // Fetch bids from the service
+        model.addAttribute("bids", bids);
         return "public/index";
     }
 }
